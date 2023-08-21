4657153864b073b79985425c87686aebf35a77e6
Andy <10527554+andyngdz@users.noreply.github.com>
2023-08-21 16:56:58 +0700

    feat: added editor item

--- lib/features/mix_editor_item/presentation/mix_editor_item.dart
+++ lib/features/mix_editor_item/presentation/mix_editor_item.dart
@@ -1,4 +1,8 @@
+import 'package:daily_mind/common_widgets/base_background_gradient.dart';
+import 'package:daily_mind/features/list_sounds/constant/sound_items.dart';
+import 'package:daily_mind/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/utils.dart';
 
 class MixEditorItem extends StatelessWidget {
   final String id;
@@ -10,6 +14,34 @@ class MixEditorItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final soundItem = soundItems.firstWhere((item) => item.id == id);
+
+    return SizedBox(
+      height: spacing(15),
+      width: context.width,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(spacing(2)),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(soundItem.image),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const BaseBackgroundGradient(),
+            Container(
+              padding: EdgeInsets.all(spacing(2)),
+              child: Text(
+                soundItem.name,
+                style: context.textTheme.titleMedium,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
