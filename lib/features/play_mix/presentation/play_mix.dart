d4faea5d73a0f2b2e77b2302558dc149b6c58195
Andy <10527554+andyngdz@users.noreply.github.com>
2023-08-29 22:05:58 +0700

    feat: added empty widget builder

--- lib/features/play_mix/presentation/play_mix.dart
+++ lib/features/play_mix/presentation/play_mix.dart
@@ -1,6 +1,7 @@
 import 'package:daily_mind/db/db.dart';
 import 'package:daily_mind/extensions/string.dart';
 import 'package:daily_mind/features/disk_player/presentation/disk_player.dart';
+import 'package:daily_mind/features/empty_widget_builder/presentation/empty_widget_builder.dart';
 import 'package:daily_mind/features/stack_background/presentation/stack_background.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -16,25 +17,27 @@ class PlayMix extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final playlist = useMemoized(() => db.getPlaylistById(playlistId));
-    final items = playlist?.items ?? [];
-    final image = items.first.id.soundItem.image;
 
-    if (playlist != null) {
-      return Scaffold(
-        body: Stack(
-          children: [
-            StackBackground(
-              child: Column(
-                children: [
-                  DiskPlayer(image: image),
-                ],
+    return EmptyWidgetBuilder(
+      data: playlist,
+      builder: (checkedPlaylist) {
+        final items = playlist?.items ?? [];
+        final image = items.first.id.soundItem.image;
+
+        return Scaffold(
+          body: Stack(
+            children: [
+              StackBackground(
+                child: Column(
+                  children: [
+                    DiskPlayer(image: image),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      );
-    }
-
-    return const SizedBox.shrink();
+            ],
+          ),
+        );
+      },
+    );
   }
 }
