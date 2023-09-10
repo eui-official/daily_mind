96c70520d43418a5b3e19a1d2c510e4b82c42a24
Andy <10527554+andyngdz@users.noreply.github.com>
2023-09-10 10:35:31 +0700

    feat: added package info plus

--- lib/features/settings/pesentation/settings_version.dart
+++ lib/features/settings/pesentation/settings_version.dart
@@ -1,16 +1,22 @@
+import 'package:daily_mind/constants/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class SettingsVersion extends StatelessWidget {
+class SettingsVersion extends HookWidget {
   const SettingsVersion({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: const Text('Phiên bản'),
-      onTap: () {},
-      trailing: const Text(
-        '3.12',
-      ),
-    );
+    return FutureBuilder(
+        future: PackageInfo.fromPlatform(),
+        builder: (context, snapshot) {
+          return ListTile(
+            title: const Text('Phiên bản'),
+            trailing: Text(
+              snapshot.data?.version ?? emptyString,
+            ),
+          );
+        });
   }
 }
