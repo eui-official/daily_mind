dee600250d538195679e63e2bea93a64ea616dc5
Andy <10527554+andyngdz@users.noreply.github.com>
2023-09-10 13:02:37 +0700

    refactor: check null improved

--- lib/features/introduction/presentation/introduction.dart
+++ lib/features/introduction/presentation/introduction.dart
@@ -1,5 +1,6 @@
 import 'package:daily_mind/db/db.dart';
 import 'package:daily_mind/features/dashboard/presentation/dashboard.dart';
+import 'package:daily_mind/features/empty_widget_builder/presentation/empty_widget_builder.dart';
 import 'package:daily_mind/features/introduction/application/introduction.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -13,33 +14,35 @@ class Introduction extends HookWidget {
   Widget build(BuildContext context) {
     final firstTime = useMemoized(() => db.getFirstTime("introduction"), []);
 
-    if (firstTime != null) {
-      return const Dashboard();
-    }
-
-    return IntroductionScreen(
-      pages: [
-        createPageViewModel(
-          context,
-          title: 'Theo sở thích của bạn',
-          body: 'Tạo một bản nhạc từ các âm thanh thiên nhiên',
-          image: 'assets/images/birds.png',
-        ),
-        createPageViewModel(
-          context,
-          title: 'Thư giãn hơn',
-          body:
-              'Âm thanh của thiên nhiên giúp thư giãn và giảm đi sự căng thẳng',
-          image: 'assets/images/relax.png',
-        ),
-      ],
-      onDone: () {
-        db.addFirstTime("introduction");
-        context.pushReplacement("/dashboard");
+    return EmptyWidgetBuilder(
+      data: firstTime,
+      builder: (safeFirstTime) {
+        return const Dashboard();
       },
-      next: const Text('Tiếp theo'),
-      done: const Text('Trang chủ'),
-      controlsPadding: const EdgeInsets.only(bottom: kToolbarHeight),
+      empty: IntroductionScreen(
+        pages: [
+          createPageViewModel(
+            context,
+            title: 'Theo sở thích của bạn',
+            body: 'Tạo một bản nhạc từ các âm thanh thiên nhiên',
+            image: 'assets/images/birds.png',
+          ),
+          createPageViewModel(
+            context,
+            title: 'Thư giãn hơn',
+            body:
+                'Âm thanh của thiên nhiên giúp thư giãn và giảm đi sự căng thẳng',
+            image: 'assets/images/relax.png',
+          ),
+        ],
+        onDone: () {
+          db.addFirstTime("introduction");
+          context.pushReplacement("/dashboard");
+        },
+        next: const Text('Tiếp theo'),
+        done: const Text('Trang chủ'),
+        controlsPadding: const EdgeInsets.only(bottom: kToolbarHeight),
+      ),
     );
   }
 }
