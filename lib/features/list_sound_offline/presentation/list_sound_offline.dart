934169c000766002604831f900e2bb6d513ec1fd
Andy <10527554+andyngdz@users.noreply.github.com>
2023-09-20 00:32:21 +0700

    refactor: removed new mix provider

--- lib/features/list_sound_offline/presentation/list_sound_offline.dart
+++ lib/features/list_sound_offline/presentation/list_sound_offline.dart
@@ -1,22 +1,31 @@
 import 'package:daily_mind/common_widgets/base_list_sound.dart';
 import 'package:daily_mind/constants/sound_items.dart';
+import 'package:daily_mind/features/new_mix/presentation/new_mix_provider.dart';
 import 'package:daily_mind/features/sound_card_offline/presentation/sound_card_offline.dart';
 import 'package:daily_mind/features/tutorial/constant/constant.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ListSoundOffline extends StatelessWidget {
+class ListSoundOffline extends HookConsumerWidget {
   const ListSoundOffline({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final newMixSelectedNotifier = ref.read(newMixProvider.notifier);
+    final newMixSelectedState = ref.watch(newMixProvider);
+
     return BaseListSound(
       title: 'naturalSounds'.tr(),
       items: soundOfflineItems,
       onSoundBuilder: (context, index, item) {
         return SoundCardOffline(
           backgroundKey: index == 0 ? soundKey : ValueKey(item.id),
+          isSelected: newMixSelectedState.selectedIds.contains(item.id),
           item: item,
+          onDeleted: newMixSelectedNotifier.onDeleted,
+          onSelected: newMixSelectedNotifier.onSelected,
+          selectingId: newMixSelectedState.selectingId,
         );
       },
     );
--- lib/features/sound_card_offline/presentation/sound_card_offline.dart
+++ lib/features/sound_card_offline/presentation/sound_card_offline.dart
@@ -1,7 +1,6 @@
 import 'package:daily_mind/common_domains/sound_offline_item.dart';
 import 'package:daily_mind/common_providers/audio_offline_player_provider.dart';
 import 'package:daily_mind/common_widgets/base_sound_card.dart';
-import 'package:daily_mind/features/new_mix/presentation/new_mix_provider.dart';
 import 'package:daily_mind/features/sound_card/presentation/sound_card_item_background.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
@@ -9,12 +8,20 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SoundCardOffline extends HookConsumerWidget {
+  final bool isSelected;
   final Key? backgroundKey;
   final SoundOfflineItem item;
+  final String selectingId;
+  final ValueChanged<String> onDeleted;
+  final ValueChanged<String> onSelected;
 
   const SoundCardOffline({
     super.key,
+    required this.isSelected,
     required this.item,
+    required this.onDeleted,
+    required this.onSelected,
+    required this.selectingId,
     this.backgroundKey,
   });
 
@@ -22,17 +29,12 @@ class SoundCardOffline extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final audioOfflinePlayerMemoized =
         useMemoized(() => audioOfflinePlayerProvider(item.id));
-
     final audioOfflinePlayerNotifier =
         ref.watch(audioOfflinePlayerMemoized.notifier);
     final audioOfflinePlayerState = ref.watch(audioOfflinePlayerMemoized);
-    final newMixSelectedNotifier = ref.read(newMixProvider.notifier);
-    final newMixSelectedState = ref.watch(newMixProvider);
-
-    final isSelected = newMixSelectedState.selectedIds.contains(item.id);
 
     final onTap = useCallback(() {
-      newMixSelectedNotifier.onSelected(item.id);
+      onSelected(item.id);
 
       if (audioOfflinePlayerState.isPlaying) {
         audioOfflinePlayerNotifier.stop();
@@ -45,19 +47,19 @@ class SoundCardOffline extends HookConsumerWidget {
     ]);
 
     useEffect(() {
-      if (newMixSelectedState.selectingId != item.id) {
+      if (selectingId != item.id) {
         audioOfflinePlayerNotifier.stop();
       }
 
       return () {};
-    }, [newMixSelectedState.selectingId]);
+    }, [selectingId]);
 
     return BaseSoundCard(
       isPlaying: audioOfflinePlayerState.isPlaying,
       isLoading: audioOfflinePlayerState.isLoading,
       isSelected: isSelected,
       onTap: onTap,
-      onDeleted: () => newMixSelectedNotifier.onDeleted(item.id),
+      onDeleted: () => onDeleted(item.id),
       image: SoundCardItemBackground(
         key: backgroundKey,
         imageProvider: AssetImage(item.image),
