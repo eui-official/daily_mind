93a76cf90fcf3aae7beae3b4fe7f9faff434a451
Andy <10527554+andyngdz@users.noreply.github.com>
2023-10-06 20:30:40 +0700

    feat: added bird loading

--- lib/features/dashboard/presentation/dashboard.dart
+++ lib/features/dashboard/presentation/dashboard.dart
@@ -1,5 +1,8 @@
+import 'package:daily_mind/common_providers/config_provider.dart';
+import 'package:daily_mind/constants/constants.dart';
 import 'package:daily_mind/features/app_navigation_bar/presentation/app_navigation_bar.dart';
 import 'package:daily_mind/features/app_navigation_bar/presentation/app_navigation_bar_provider.dart';
+import 'package:daily_mind/features/bird_loading/presentation/bird_loading.dart';
 import 'package:daily_mind/features/mini_player/presentation/mini_player_switcher.dart';
 import 'package:daily_mind/features/offline_list/presentation/offline_list.dart';
 import 'package:daily_mind/features/online_list/presentation/online_list.dart';
@@ -16,6 +19,7 @@ class Dashboard extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final configState = ref.watch(configProvider);
     final appNavigationBarState = ref.watch(appNavigationBarProvider);
 
     final children = useMemoized(
@@ -26,21 +30,35 @@ class Dashboard extends HookConsumerWidget {
             ],
         []);
 
-    return Scaffold(
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            IndexedStack(
-              index: appNavigationBarState.index,
-              children: children,
-            ),
-            const MiniPlayerSwitcher(),
-          ],
+    final child = useMemoized(() {
+      if (configState.isLoading) {
+        return const BirdLoading();
+      }
+
+      return Scaffold(
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.light,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              IndexedStack(
+                index: appNavigationBarState.index,
+                children: children,
+              ),
+              const MiniPlayerSwitcher(),
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: const AppNavigationBar(),
+        bottomNavigationBar: const AppNavigationBar(),
+      );
+    }, [configState.isLoading]);
+
+    return AnimatedSwitcher(
+      duration: mediumDuration,
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        return FadeTransition(opacity: animation, child: child);
+      },
+      child: child,
     );
   }
 }
