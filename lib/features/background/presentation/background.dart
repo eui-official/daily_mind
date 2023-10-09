89bf2bec427533c8a687ea2f5453b3e1743ead6b
Andy <10527554+andyngdz@users.noreply.github.com>
2023-10-09 10:41:44 +0700

    feat: added BaseAnimatedOpacity

--- lib/features/background/presentation/background.dart
+++ lib/features/background/presentation/background.dart
@@ -1,8 +1,7 @@
+import 'package:daily_mind/common_widgets/base_animated_opacity.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:transparent_image/transparent_image.dart';
 
-class Background extends HookWidget {
+class Background extends StatelessWidget {
   final ImageProvider image;
 
   const Background({
@@ -12,18 +11,19 @@ class Background extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final fadeInImage = useMemoized(() {
-      return FadeInImage(
-        placeholder: MemoryImage(kTransparentImage),
-        image: image,
-        fit: BoxFit.cover,
-        placeholderFit: BoxFit.cover,
-      );
-    }, [image]);
-
     return Stack(
       children: [
-        Positioned.fill(child: fadeInImage),
+        BaseAnimatedOpacity(
+          valueKey: ValueKey(image),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: image,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
         Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
