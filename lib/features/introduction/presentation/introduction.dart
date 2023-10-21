020f576c0fd0b8d30b270b1bbe4521ac28c5a812
Andy <10527554+andyngdz@users.noreply.github.com>
2023-10-21 15:40:05 +0700

    fix: reanimated text kit

--- lib/features/introduction/presentation/introduction.dart
+++ lib/features/introduction/presentation/introduction.dart
@@ -2,6 +2,7 @@ import 'package:daily_mind/db/db.dart';
 import 'package:daily_mind/features/dashboard/presentation/dashboard.dart';
 import 'package:daily_mind/features/empty_widget_builder/presentation/empty_widget_builder.dart';
 import 'package:daily_mind/features/introduction/application/introduction.dart';
+import 'package:daily_mind/features/setting_watcher/presentation/setting_watcher.dart';
 import 'package:daily_mind/features/settings/pesentation/settings_language.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
@@ -22,34 +23,40 @@ class Introduction extends HookWidget {
         return const Dashboard();
       },
       empty: Scaffold(
-        body: IntroductionScreen(
-          pages: [
-            createPageViewModel(
-              context,
-              title: 'hello'.tr(),
-              bodyWidget: const SettingsLanguage(),
-              image: 'assets/images/wonderful-day.png',
-            ),
-            createPageViewModel(
-              context,
-              title: 'byYourFavorites'.tr(),
-              body: 'createANewMixFromNaturalSounds'.tr(),
-              image: 'assets/images/birds.png',
-            ),
-            createPageViewModel(
-              context,
-              title: 'moreRelaxing'.tr(),
-              body: 'relaxMoreWithStories'.tr(),
-              image: 'assets/images/relax.png',
-            ),
-          ],
-          onDone: () {
-            db.addFirstTime("introduction");
-            context.pushReplacement("/dashboard");
+        body: SettingWatcher(
+          onWidgetSettingBuilder: (settings) {
+            return IntroductionScreen(
+              key: ValueKey(settings?.value),
+              pages: [
+                createPageViewModel(
+                  context,
+                  title: 'hello'.tr(),
+                  bodyWidget: const SettingsLanguage(),
+                  image: 'assets/images/wonderful-day.png',
+                ),
+                createPageViewModel(
+                  context,
+                  title: 'byYourFavorites'.tr(),
+                  body: 'createANewMixFromNaturalSounds'.tr(),
+                  image: 'assets/images/birds.png',
+                ),
+                createPageViewModel(
+                  context,
+                  title: 'moreRelaxing'.tr(),
+                  body: 'relaxMoreWithStories'.tr(),
+                  image: 'assets/images/relax.png',
+                ),
+              ],
+              onDone: () {
+                db.addFirstTime("introduction");
+                context.pushReplacement("/dashboard");
+              },
+              next: Text('next'.tr()),
+              done: Text('dashboard'.tr()),
+              controlsPadding: const EdgeInsets.only(bottom: kToolbarHeight),
+            );
           },
-          next: Text('next'.tr()),
-          done: Text('dashboard'.tr()),
-          controlsPadding: const EdgeInsets.only(bottom: kToolbarHeight),
+          type: 'language',
         ),
       ),
     );
