1f80b8de1a0d0707804f0b95711d9b4b4b85324e
Andy <10527554+andyngdz@users.noreply.github.com>
2023-10-29 23:21:46 +0700

    fix: auto close modal if internet back

--- lib/features/internet_watcher/presentation/internet_watcher.dart
+++ lib/features/internet_watcher/presentation/internet_watcher.dart
@@ -1,8 +1,11 @@
+import 'package:daily_mind/common_applications/safe_builder.dart';
+import 'package:daily_mind/common_hooks/use_effect_delayed.dart';
 import 'package:daily_mind/common_widgets/base_internet_check/presentation/base_internet_check_provider.dart';
-import 'package:daily_mind/common_widgets/base_null_builder.dart';
-import 'package:daily_mind/constants/constants.dart';
 import 'package:daily_mind/features/internet_watcher/presentation/internet_watcher_content.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:get/utils.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class InternetWatcher extends HookConsumerWidget {
@@ -15,24 +18,42 @@ class InternetWatcher extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final isOpen = useState(false);
     final baseInternetCheckState = ref.watch(baseInternetCheckNotifierProvider);
 
-    return BaseNullBuilder(
-      value: baseInternetCheckState.value,
-      builder: (state) {
-        return Stack(
-          key: ValueKey(state.connectivityResult),
-          children: [
-            child,
-            AnimatedSwitcher(
-              duration: kThemeChangeDuration,
-              child: state.isDisconnected
-                  ? const InternetWatcherContent()
-                  : emptyWidget,
-            ),
-          ],
+    final onShowInternetDisconnected = useCallback(
+      () {
+        safeValueBuilder(
+          baseInternetCheckState.value,
+          (state) async {
+            if (state.isDisconnected) {
+              isOpen.value = true;
+
+              await showModalBottomSheet(
+                context: context,
+                backgroundColor: context.theme.colorScheme.background,
+                elevation: 0,
+                builder: (context) {
+                  return const InternetWatcherContent();
+                },
+              );
+
+              isOpen.value = false;
+            } else {
+              if (isOpen.value) {
+                context.pop();
+              }
+            }
+          },
         );
       },
+      [context, baseInternetCheckState],
     );
+
+    useEffectDelayed(() {
+      onShowInternetDisconnected();
+    }, [onShowInternetDisconnected]);
+
+    return child;
   }
 }
--- lib/features/internet_watcher/presentation/internet_watcher_content.dart
+++ lib/features/internet_watcher/presentation/internet_watcher_content.dart
@@ -1,41 +1,34 @@
-import 'package:daily_mind/common_widgets/base_circular_indicator.dart';
-import 'package:daily_mind/common_widgets/base_marquee.dart';
-import 'package:daily_mind/theme/common.dart';
+import 'package:daily_mind/common_widgets/base_header_with_description.dart';
 import 'package:daily_mind/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:get/utils.dart';
+import 'package:go_router/go_router.dart';
 
 class InternetWatcherContent extends StatelessWidget {
   const InternetWatcherContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            context.theme.colorScheme.background,
-            context.theme.colorScheme.background.withOpacity(0.9),
-          ],
-          begin: Alignment.bottomCenter,
-          end: Alignment.topCenter,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: spacing(2),
+          vertical: spacing(3),
         ),
-      ),
-      child: FractionallySizedBox(
-        widthFactor: 0.5,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: space(
-            [
-              const Flexible(child: BaseCircularIndicator(size: 4)),
-              SizedBox(
-                height: spacing(3),
-                child: const BaseMarquee(text: 'Không có kết nối'),
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Flexible(
+              child: BaseHeaderWithDescription(
+                name: 'Không có kết nối internet',
+                description:
+                    'Điều này có thể gây ảnh hưởng đến trãi nghiệm của bạn',
+                headerAction: IconButton(
+                  onPressed: context.pop,
+                  icon: const Icon(Icons.close),
+                ),
               ),
-            ],
-            height: spacing(2),
-          ),
+            )
+          ],
         ),
       ),
     );
