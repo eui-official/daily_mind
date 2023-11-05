8ed1390fb6b962a4d2b8c976ea5b3dc7a12e165a
Andy <10527554+andyngdz@users.noreply.github.com>
2023-11-05 23:59:22 +0700

    fix: range error when switch onlin playlist

--- lib/features/offline_player/presentation/offline_player_bottom.dart
+++ lib/features/offline_player/presentation/offline_player_bottom.dart
@@ -10,7 +10,6 @@ import 'package:daily_mind/theme/theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:get/utils.dart' hide Trans;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class OfflinePlayerBottom extends HookConsumerWidget {
