58171f23a5fac495daa5c32169711bfd5bb6b040
Andy <10527554+andyngdz@users.noreply.github.com>
2023-11-07 21:15:58 +0700

    feat: updated timer

--- lib/features/focus_mode_session/presentation/focus_mode_session.dart
+++ lib/features/focus_mode_session/presentation/focus_mode_session.dart
@@ -1,5 +1,6 @@
 import 'package:daily_mind/constants/constants.dart';
 import 'package:daily_mind/db/schemas/pomodoro.dart';
+import 'package:daily_mind/features/focus_mode_actions/presentation/focus_mode_actions.dart';
 import 'package:daily_mind/features/focus_mode_timer/presentation/focus_mode_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:get/utils.dart';
@@ -19,6 +20,7 @@ class FocusModeSession extends StatelessWidget {
         alignment: Alignment.center,
         padding: const EdgeInsets.symmetric(vertical: kToolbarHeight * 2),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
               pomodoro.title ?? emptyString,
@@ -27,6 +29,7 @@ class FocusModeSession extends StatelessWidget {
               ),
             ),
             const FocusModeTimer(),
+            const FocusModeActions(),
           ],
         ),
       ),
