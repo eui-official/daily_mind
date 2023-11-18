8044ba777497058e83c726afe2e25b7a08e9cbd8
Andy <10527554+andyngdz@users.noreply.github.com>
2023-11-18 20:57:45 +0700

    refactor: updated colors

--- lib/features/mix_collection_item/presentation/mix_collection_item.dart
+++ lib/features/mix_collection_item/presentation/mix_collection_item.dart
@@ -1,12 +1,12 @@
-import 'package:daily_mind/common_providers/base_audio_handler_provider.dart';
 import 'package:daily_mind/common_widgets/base_card/presentation/base_card.dart';
-import 'package:daily_mind/common_widgets/base_mini_player/presentation/base_mini_player_provider.dart';
 import 'package:daily_mind/common_widgets/base_spacing/presentation/base_spacing_container.dart';
 import 'package:daily_mind/constants/constants.dart';
 import 'package:daily_mind/db/schemas/playlist.dart';
 import 'package:daily_mind/extensions/string.dart';
+import 'package:daily_mind/theme/theme.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:get/get.dart' hide Trans;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class MixCollectionItem extends HookConsumerWidget {
@@ -19,12 +19,11 @@ class MixCollectionItem extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final baseBackgronudHandler = ref.watch(baseBackgroundHandlerProvider);
-    final baseMiniPlayerNotifier = ref.read(baseMiniPlayerProvider.notifier);
-
     final items = playlist.items ?? [];
     final firstItem = items.first;
     final firstAudioOffline = firstItem.id.onGetOfflineAudio;
+    final sounds =
+        items.map((item) => item.id.onGetOfflineAudio.name.tr()).join(', ');
 
     return BaseCard(
       content: BaseSpacingContainer(
@@ -38,6 +37,12 @@ class MixCollectionItem extends HookConsumerWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            Text(
+              sounds,
+              style: context.textTheme.bodySmall?.copyWith(
+                color: secondaryTextColor,
+              ),
+            )
           ],
         ),
       ),
