f02db851ead9b88b9b32f3a5167edd06c31c6e0b
Andy <10527554+andyngdz@users.noreply.github.com>
2023-11-21 00:34:52 +0700

    fix: select task

--- lib/features/focus_mode_task_item/presentation/focus_mode_task_item.dart
+++ lib/features/focus_mode_task_item/presentation/focus_mode_task_item.dart
@@ -1,11 +1,16 @@
+import 'package:daily_mind/common_applications/base_audio_handler/base_audio_handler.dart';
+import 'package:daily_mind/common_providers/base_audio_handler_provider.dart';
 import 'package:daily_mind/constants/constants.dart';
 import 'package:daily_mind/db/schemas/task.dart';
 import 'package:daily_mind/features/focus_mode_delete_sliable/presentation/focus_mode_delete_sliable.dart';
 import 'package:daily_mind/theme/common.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class FocusModeTaskItem extends StatelessWidget {
+class FocusModeTaskItem extends HookConsumerWidget {
   final Task task;
 
   const FocusModeTaskItem({
@@ -14,7 +19,17 @@ class FocusModeTaskItem extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final baseBackgroundHandler = ref.watch(baseBackgroundHandlerProvider);
+
+    final onSelectTask = useCallback(
+      () {
+        baseBackgroundHandler.onTaskInit(task);
+        context.pop();
+      },
+      [],
+    );
+
     return ClipRRect(
       borderRadius: circularRadius(2),
       child: Material(
@@ -22,7 +37,7 @@ class FocusModeTaskItem extends StatelessWidget {
         child: FocusModeDeleteSliable(
           id: task.id,
           child: ListTile(
-            onTap: () {},
+            onTap: onSelectTask,
             title: Text(
               task.title ?? emptyString,
               style: context.textTheme.bodyLarge?.copyWith(
