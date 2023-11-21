399813f0a0b4886d5be3d6e4977c39f325cd20dd
Andy <10527554+andyngdz@users.noreply.github.com>
2023-11-21 09:27:58 +0700

    refactor: added isSelected

--- lib/features/focus_mode_task_item/presentation/focus_mode_task_item.dart
+++ lib/features/focus_mode_task_item/presentation/focus_mode_task_item.dart
@@ -1,8 +1,10 @@
 import 'package:daily_mind/common_applications/base_audio_handler/base_audio_handler.dart';
 import 'package:daily_mind/common_providers/base_audio_handler_provider.dart';
 import 'package:daily_mind/constants/constants.dart';
+import 'package:daily_mind/db/db.dart';
 import 'package:daily_mind/db/schemas/task.dart';
 import 'package:daily_mind/features/focus_mode_delete_sliable/presentation/focus_mode_delete_sliable.dart';
+import 'package:daily_mind/features/focus_mode_task_item/presentation/focus_mode_task_item_selected.dart';
 import 'package:daily_mind/theme/common.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -21,6 +23,8 @@ class FocusModeTaskItem extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final baseBackgroundHandler = ref.watch(baseBackgroundHandlerProvider);
+    final taskCurrent = baseBackgroundHandler.taskCurrent;
+    final isSelected = taskCurrent.id == task.id;
 
     final onSelectTask = useCallback(
       () {
@@ -30,12 +34,29 @@ class FocusModeTaskItem extends HookConsumerWidget {
       [],
     );
 
+    final onDeletedTask = useCallback(
+      () {
+        db.onDeleteTask(task.id);
+
+        if (isSelected) {
+          final tasks = db.onGetTasks();
+
+          if (tasks.isNotEmpty) {
+            final firstTask = tasks.first;
+            baseBackgroundHandler.onTaskInit(firstTask);
+          }
+        }
+      },
+      [isSelected],
+    );
+
     return ClipRRect(
       borderRadius: circularRadius(2),
       child: Material(
         type: MaterialType.transparency,
         child: FocusModeDeleteSliable(
           id: task.id,
+          onDeleted: onDeletedTask,
           child: ListTile(
             onTap: onSelectTask,
             title: Text(
@@ -44,8 +65,8 @@ class FocusModeTaskItem extends HookConsumerWidget {
                 fontWeight: FontWeight.w500,
               ),
             ),
+            trailing: FocusModeTaskItemSelected(isSelected: isSelected),
           ),
-          onDeleted: () {},
         ),
       ),
     );
