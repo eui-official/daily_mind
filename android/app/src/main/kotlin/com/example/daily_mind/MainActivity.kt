bed38ef4767e7569b7f90eee47e97e4e9bd12dc0
Andy <10527554+andyngdz@users.noreply.github.com>
2023-11-26 13:35:25 +0700

    fix: android build (#161)

--- android/app/src/main/kotlin/com/example/daily_mind/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/daily_mind/MainActivity.kt
@@ -1,5 +1,14 @@
-package com.example.daily_mind
+package dev.andyng.dailymind
 
+import android.content.Context
+import com.ryanheise.audioservice.AudioServicePlugin
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity()
+
+class MainActivity:FlutterActivity() {
+  @Override
+  final public override fun provideFlutterEngine(context: Context): FlutterEngine {
+    return AudioServicePlugin.getFlutterEngine(context)
+  }
+}
