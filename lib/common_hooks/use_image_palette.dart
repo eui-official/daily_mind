55df0eb03d33cc14e77d8a833e92568ac8138c57
Andy <10527554+andyngdz@users.noreply.github.com>
2023-12-13 09:25:35 +0700

    chore: image size for extract colors is 1,1

--- lib/common_hooks/use_image_palette.dart
+++ lib/common_hooks/use_image_palette.dart
@@ -1,16 +1,14 @@
-import 'package:daily_mind/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:palette_generator/palette_generator.dart';
+
+const kImageSizeForExtractColors = Size(1, 1);
 
 AsyncSnapshot<PaletteGenerator> useImagePallete(ImageProvider image) {
   final paletteGeneratorMemoized = useMemoized(
     () => PaletteGenerator.fromImageProvider(
       image,
-      size: Size(
-        spacing(),
-        spacing(),
-      ),
+      size: kImageSizeForExtractColors,
     ),
     [image],
   );
