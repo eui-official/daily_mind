a706d41980833ed85d6f822546d27c5ea07215e9
Andy <10527554+andyngdz@users.noreply.github.com>
2023-12-13 14:59:58 +0700

    fix: slow reloading when switching between mix and collection

--- lib/features/mix_switch/presentation/mix_switch.dart
+++ lib/features/mix_switch/presentation/mix_switch.dart
@@ -1,5 +1,5 @@
-import 'package:daily_mind/common_widgets/base_animated_switcher.dart';
 import 'package:daily_mind/common_widgets/base_spacing/presentation/base_spacing_container_horizontal.dart';
+import 'package:daily_mind/common_widgets/fade_indexed_stack.dart';
 import 'package:daily_mind/features/mix_collections_switcher/presentation/mix_collections_switcher.dart';
 import 'package:daily_mind/features/mix_list_audio/presentation/mix_list_audio.dart';
 import 'package:flutter/material.dart';
@@ -17,21 +17,16 @@ class MixSwitch extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final child = useMemoized(
-      () {
-        if (index == 0) {
-          return const MixListAudio();
-        }
-
-        return MixCollectionsSwitcher(onCreateNew: onCreateNew);
-      },
-      [index],
-    );
-
     return BaseSpacingContainerHorizontal(
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: kToolbarHeight),
-        child: BaseAnimatedSwitcher(child: child),
+        child: FadeIndexedStack(
+          index: index,
+          children: [
+            const MixListAudio(),
+            MixCollectionsSwitcher(onCreateNew: onCreateNew),
+          ],
+        ),
       ),
     );
   }
