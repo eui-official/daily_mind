51251a33b78c88cccd9ee1fffcd87a4b801ea4a9
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2023-12-17 11:20:45 +0700

    feat: added background for mix player (#193)

--- lib/features/mix_player_list_item/presentation/mix_player_list_item.dart
+++ lib/features/mix_player_list_item/presentation/mix_player_list_item.dart
@@ -1,7 +1,9 @@
+import 'package:daily_mind/common_widgets/base_content_header.dart';
 import 'package:daily_mind/features/mix/domain/mix_item.dart';
 import 'package:daily_mind/features/mix_player_item/presentation/mix_player_item.dart';
 import 'package:daily_mind/theme/common.dart';
 import 'package:daily_mind/theme/theme.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -24,6 +26,10 @@ class MixPlayerListItem extends HookWidget {
       );
     }, [mixItems]);
 
-    return Column(children: children);
+    return BaseContentHeader(
+      title: 'Danh sách âm thanh'.tr(),
+      spacingSize: 4,
+      child: Column(children: children),
+    );
   }
 }
