56f6d17d5dc037cdaad6233c72b704eb2adb165f
Andy <10527554+andyngdz@users.noreply.github.com>
2024-01-01 12:24:14 +0700

    feat: added play button for playlist

--- lib/features/playlist_details/presentation/playlist_details.dart
+++ lib/features/playlist_details/presentation/playlist_details.dart
@@ -1,11 +1,6 @@
-import 'package:daily_mind/common_applications/base_audio_handler/base_audio_handler.dart';
-import 'package:daily_mind/common_providers/base_audio_handler_provider.dart';
 import 'package:daily_mind/common_widgets/base_audios_ids_builder/presentation/base_audio_ids_builder.dart';
-import 'package:daily_mind/common_widgets/base_mini_player/domain/mini_player_state.dart';
-import 'package:daily_mind/common_widgets/base_mini_player/presentation/base_mini_player_provider.dart';
 import 'package:daily_mind/common_widgets/base_null_builder.dart';
 import 'package:daily_mind/constants/constants.dart';
-import 'package:daily_mind/constants/enums.dart';
 import 'package:daily_mind/db/db.dart';
 import 'package:daily_mind/features/playlist_details/presentation/playlist_details_content.dart';
 import 'package:flutter/material.dart';
@@ -22,8 +17,6 @@ class PlaylistDetails extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final onlinePlaylist = db.onGetOnlinePlaylist(playlistId);
-    final baseBackgroundHandler = ref.watch(baseBackgroundHandlerProvider);
-    final baseMiniPlayerNotifier = ref.read(baseMiniPlayerProvider.notifier);
 
     return BaseNullBuilder(
       value: onlinePlaylist,
@@ -32,20 +25,6 @@ class PlaylistDetails extends HookConsumerWidget {
           ids: safeOnlinePlaylist.itemIds,
           builder: (audios) {
             return Scaffold(
-              floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-              floatingActionButton: FloatingActionButton(
-                onPressed: () async {
-                  await baseBackgroundHandler.onInitOnline(audios);
-
-                  baseMiniPlayerNotifier.onUpdateState(
-                    const MiniPlayerState(
-                      isShow: true,
-                      audioType: AudioTypes.online,
-                    ),
-                  );
-                },
-                child: const Icon(Icons.play_arrow),
-              ),
               body: PlaylistDetailsContent(
                 title: safeOnlinePlaylist.title ?? kEmptyString,
                 audios: audios,
