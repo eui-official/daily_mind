906ab8cc5aa2793a849769b2ab6c16930b872181
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2024-01-02 11:23:24 +0700

    feat: update online player title from playlist (#218)

--- lib/features/playlist_details/presentation/playlist_details_list_audio.dart
+++ lib/features/playlist_details/presentation/playlist_details_list_audio.dart
@@ -2,36 +2,50 @@ import 'package:daily_mind/common_domains/audio.dart';
 import 'package:daily_mind/common_widgets/base_sliable.dart';
 import 'package:daily_mind/common_widgets/base_spacing/presentation/base_spacing_container_horizontal.dart';
 import 'package:daily_mind/db/db.dart';
+import 'package:daily_mind/db/schemas/online_playlist.dart';
 import 'package:daily_mind/features/online_item/presentation/online_item.dart';
 import 'package:daily_mind/features/online_item/presentation/online_title.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class PlaylistDetailsListAudio extends HookWidget {
-  final int playlistId;
+  final OnlinePlaylist onlinePlaylist;
   final List<Audio> audios;
 
   const PlaylistDetailsListAudio({
     super.key,
-    required this.playlistId,
+    required this.onlinePlaylist,
     required this.audios,
   });
 
   @override
   Widget build(BuildContext context) {
+    final currentAudios = useState<List<Audio>>([]);
+
     final onDeleted = useCallback(
       (Audio audio) {
         db.onDeleteSongInPlaylist(
-          playlistId,
+          onlinePlaylist,
           audio.id,
         );
+
+        final newAudios = List<Audio>.from(currentAudios.value);
+        newAudios.remove(audio);
+
+        currentAudios.value = newAudios;
       },
-      [playlistId],
+      [onlinePlaylist],
     );
+
+    useEffect(() {
+      currentAudios.value = audios;
+
+      return () {};
+    }, [audios]);
 
     return BaseSpacingContainerHorizontal(
       child: Column(
-        children: audios.map((audio) {
+        children: currentAudios.value.map((audio) {
           return BaseSliable(
             id: audio.id,
             onDeleted: () => onDeleted(audio),
