175b29148c4e9c8d274434213c90a3d53a84a725
Andy <10527554+andyngdz@users.noreply.github.com>
2024-03-23 11:05:06 +0700

    feat: added notifications service

--- lib/features/sleep_mode/presentation/sleep_mode_provider.dart
+++ lib/features/sleep_mode/presentation/sleep_mode_provider.dart
@@ -1,9 +1,9 @@
 import 'package:alarm/alarm.dart';
 import 'package:alarm/model/alarm_settings.dart';
+import 'package:daily_mind/common_applications/notifications.dart';
 import 'package:daily_mind/features/sleep_mode/domain/sleep_mode_state.dart';
 import 'package:daily_mind/features/sleep_mode_time_clock/presentation/sleep_mode_time_clock_provider.dart';
 import 'package:daily_mind/features/sleep_mode_wake_up_audios/presentation/sleep_mode_wake_up_audios_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'sleep_mode_provider.g.dart';
@@ -22,39 +22,33 @@ class SleepModeNotifier extends _$SleepModeNotifier {
   }
 
   void onStartSleeping() async {
-    final status = await Permission.notification.status;
+    final status = await notifications.onCheckPermissions();
 
-    if (status.isDenied) {
-      alarmPrint('Requesting notification permission...');
-      final res = await Permission.notification.request();
-      alarmPrint(
-        'Notification permission ${res.isGranted ? '' : 'not '}granted.',
+    if (status) {
+      final sleepModeTimeClockState =
+          ref.read(sleepModeTimeClockNotifierProvider);
+      final sleepModeWakeUpAudiosState =
+          ref.read(sleepModeWakeUpAudiosNotifierProvider);
+      const title = 'Xin chào,';
+      const body = 'Đã đến giờ thức dậy';
+
+      await Alarm.setNotificationOnAppKillContent(title, body);
+
+      final alarmSettings = AlarmSettings(
+        id: 42,
+        dateTime: sleepModeTimeClockState.endTime,
+        assetAudioPath:
+            sleepModeWakeUpAudiosState.wakeUpOfflineAudio.sources.first,
+        loopAudio: true,
+        vibrate: true,
+        volume: 0.8,
+        fadeDuration: 30,
+        notificationTitle: title,
+        notificationBody: body,
+        enableNotificationOnKill: true,
       );
+
+      await Alarm.set(alarmSettings: alarmSettings);
     }
-
-    final sleepModeTimeClockState =
-        ref.read(sleepModeTimeClockNotifierProvider);
-    final sleepModeWakeUpAudiosState =
-        ref.read(sleepModeWakeUpAudiosNotifierProvider);
-    const title = 'Đã đến giờ thức dậy!';
-    const body = 'Hãy bắt đầu';
-
-    await Alarm.setNotificationOnAppKillContent(title, body);
-
-    final alarmSettings = AlarmSettings(
-      id: 42,
-      dateTime: sleepModeTimeClockState.endTime,
-      assetAudioPath:
-          sleepModeWakeUpAudiosState.wakeUpOfflineAudio.sources.first,
-      loopAudio: true,
-      vibrate: true,
-      volume: 0.8,
-      fadeDuration: 30,
-      notificationTitle: title,
-      notificationBody: body,
-      enableNotificationOnKill: true,
-    );
-
-    await Alarm.set(alarmSettings: alarmSettings);
   }
 }
